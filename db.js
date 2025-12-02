2f5bddb855c3039faa10870fd7b8eff5c9368108
juanmanuelquintero <juanmanuelquinterol013@gmail.com>
2025-12-02 16:16:28 -0500

    Actualizacion

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "shinkansen.proxy.rlwy.net",
-  user: "root",
-  password: "wbvYIEhIJJwRkMQiZLiNSUDwEdmQgYMp",
-  database: "railway",
-  port: 41100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
